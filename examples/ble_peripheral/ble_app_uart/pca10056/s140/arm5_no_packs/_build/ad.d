da94e8d5834fd53c809e001b60fa40c7c135f09d
zhangmuyanag <279401783@qq.com>
2019-04-30 10:25:46 +0800

    first commit

---
+++ examples/ble_peripheral/ble_app_uart/pca10056/s140/arm5_no_packs/_build/ad.d
@@ -0,0 +1,17 @@
+.\_build\ad.o: ad.c
+.\_build\ad.o: ad.h
+.\_build\ad.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\ad.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\_build\ad.o: ..\..\..\..\..\..\components\libraries\util\nordic_common.h
+.\_build\ad.o: ..\..\..\..\..\..\modules\nrfx\mdk\nrf.h
+.\_build\ad.o: ..\..\..\..\..\..\modules\nrfx\mdk\nrf52840.h
+.\_build\ad.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm4.h
+.\_build\ad.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
+.\_build\ad.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
+.\_build\ad.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
+.\_build\ad.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmSimd.h
+.\_build\ad.o: ..\..\..\..\..\..\modules\nrfx\mdk\system_nrf52840.h
+.\_build\ad.o: ..\..\..\..\..\..\modules\nrfx\mdk\nrf52840_bitfields.h
+.\_build\ad.o: ..\..\..\..\..\..\modules\nrfx\mdk\nrf51_to_nrf52840.h
+.\_build\ad.o: ..\..\..\..\..\..\modules\nrfx\mdk\nrf52_to_nrf52840.h
+.\_build\ad.o: ..\..\..\..\..\..\modules\nrfx\mdk\compiler_abstraction.h
